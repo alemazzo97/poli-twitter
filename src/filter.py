7e26d02ecff8429b785a92661565c6d29b239cc4
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2020-01-05 19:17:58 +0100

    Update filter.py

--- src/filter.py
+++ src/filter.py
@@ -6,26 +6,37 @@ from os.path import isfile, join
 
 
 mypath = "../../Tweets/"  # dove si trovano i tweet di ogni utente
+# path dove salvare i file excel contenenti i tweet puliti
+path_to_write = "../dist/clean_users/"
+
 # ottengo una lista dei nomi di tutti i file presenti nella cartella specificata
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-# filtro sui tweet in ingresso
+# FILTRI
+# Data di partenza
 starting_date = "2019-01-01 00:00:00"  # inizio 2019
-# filtro sul numero di tweet dell'utente in input
+ending_date = "2019-12-31 23:59:59"  # fine 2019
+# Minimo numero di Tweet totali accettati
 tweet_input_threshold = 100
-# filtro sul numero di tweet dell'utente dopo il filtro
+# Parole chiave da ricercare
+keywords = ("salvini", "di maio", "renzi", "berlusconi")
+# Minimo numero di Tweet "politici" accettati
 tweet_filtered_threshold = 50
-# lista di parole chiave da cercare
-keywords = ("salvini", "di maio")
-# path dove salvare i file excel
-path_to_write = "../dist/clean_users/"
 
 
-def hasKeywords(text):
+def hasKeywords(text):  # funzione per la ricerca di parole chiave
     for k in keywords:
         if k in text:
             return True
     return False
+
+
+def filterTweet(tweet):
+    return tweet["created_at"] > starting_date and tweet["created_at"] < ending_date and hasKeywords(tweet["full_text"])
+
+
+def filterUserTweets(tweets):
+    return len(tweets) > tweet_filtered_threshold
 
 
 def insertTweets(tweets, filename):  # funzione per inserire un tweet nel database
@@ -35,21 +46,18 @@ def insertTweets(tweets, filename):  # funzione per inserire un tweet nel databa
 
 
 for i in range(len(files)):  # per ogni file
-    file = files[i]
-    data = pd.read_csv(mypath+file, sep=",",
-                       encoding="utf8")  # leggo con pandas
-    df = pd.DataFrame(data)  # estraggo il dataframe
-    if len(df) >= tweet_input_threshold:  # filtro sul numero iniziale dei tweet
-        tweets = []  # variabile per contenere i tweet formattati correttamente per l'inserimento nel db
-        for i in range(1, len(df)):  # per ogni tweet dell'utente
-            # filtro sulla data di creazione
-            if df.iloc[i]["created_at"] > starting_date and hasKeywords(df.iloc[i]["full_text"]):
-                tweets.append((str(df.iloc[i]["id"]), str(df.iloc[i]["created_at"]), str(df.iloc[i]["favorites"]), str(
-                    df.iloc[i]["retweets"]), str(df.iloc[i]["source"]), str(df.iloc[i]["full_text"])))  # aggiungo il tweet alla lista da salvare su db
-            pass
-        # ho completato la lista dei tweet per questo utente: lancio la scrittura su csv della lista
-        if len(tweets) > tweet_filtered_threshold:
-            insertTweets(tweets[:50], file)
-    pass
+    file_name = files[i]
+    data = pd.read_csv(mypath+file_name, sep=",",
+                       encoding="utf8")
+    df = pd.DataFrame(data)
+    tweets = []
+    for i in range(1, len(df)):
+        tweet = df.iloc[i]  # per ogni tweet dell'utente
+        if filterTweet(tweet):  # filtro secondo le regole definite
+            tweets.append(tweet)
+    # ho completato la lista dei tweet per questo utente: lancio la scrittura su csv della lista
+    if filterUserTweets(tweets):  # filtro sul numero di tweet filtrati
+        # taglio e prendo solo i primi 50 tweet
+        insertTweets(tweets[:50], file_name)
 
 print("Done with 0 errors")
