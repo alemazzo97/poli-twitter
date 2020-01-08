9c418539ad33e6c2e7c8fb1706f30495fd3f2eb6
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2020-01-08 09:15:54 +0100

    Update sentiment.py

--- src/sentiment.py
+++ src/sentiment.py
@@ -4,7 +4,9 @@ from googletrans import Translator
 translator = Translator()
 
 # frase = "Grazie sindaco! Quando prevede interventi in viale Corsica e strade limitrofe? Venga a dare una occhiata, credo sia importante."
-data = pd.read_csv("../dist/clean_users/100_lyon_tweets.csv", sep=",", encoding="utf8")
+your_path = "../dist/clean_users/"
+your_file = "100_lyon_tweets.csv"
+data = pd.read_csv(your_path+your_file, sep=",", encoding="utf8")
 df = pd.DataFrame(data)
 print(df)
 
@@ -13,20 +15,14 @@ def deEmojify(inputString):
     return inputString.encode('ascii', 'ignore').decode('ascii')
 
 
-# def removeLink(word):
-#     if word.starts_with("http")
-#     return ""
-#     else return word
-
-
 sum_of_polarities = 0
-
-for i in range(0, 50):
-    tweet_ita = df.iloc[i][5]
-    print(tweet_ita)
+how_many_users = 50
+for i in range(0, how_many_users):
+    tweet_ita = df.iloc[i][5]  # posizione del full_text
+    # print(tweet_ita)
     tweet_ita = deEmojify(tweet_ita)
     tweet = translator.translate(tweet_ita, dest='en').text
-    print(tweet)
+    # print(tweet)
     sum_of_polarities += TextBlob(tweet).sentiment.polarity
 
 print(sum_of_polarities/len(df))
